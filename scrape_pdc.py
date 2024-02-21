a7656edc7af0cf470b99fef687404d97e8bf5fbe
Anthony Graffigna <graffigna.a@husky.neu.edu>
2024-02-21 15:32:16 -0500

    Move bills list cleaning into main scraper

--- scrape_pdc.py
+++ scrape_pdc.py
@@ -3,6 +3,31 @@ import datetime
 import pandas as pd
 from bs4 import BeautifulSoup as soup 
 
+# Takes a raw scrape of a list of bills/issues as a string and returns a clean list of bills separated by a comma and a space
+def clean_bills_list(bills_raw):
+	print(bills_raw)
+	bills_upper = str(bills_raw).upper()
+	remove_hb_sb_spaces = bills_upper.replace("HB ", "HB").replace("SB ", "SB")
+	delimit_with_spaces = remove_hb_sb_spaces.replace(",", " ").replace("/", " ").replace(":", " ").replace(";", " ").replace("-", " ").replace(", ", " ").replace(")", "").replace("(", "")
+	bills_list = delimit_with_spaces.split(" ")
+	filter_nonnumbers = filter(filter_numbers, bills_list)
+	filter_nonhb_sb = filter(filter_hb_sb, filter_nonnumbers)
+	return list(filter_nonhb_sb)
+
+def filter_numbers(x):
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	for c in x:
+		if c in numbers:
+			return True
+	return False
+
+def filter_hb_sb(x):
+	if "HB" in x:
+		return True
+	elif "SB" in x:
+		return True
+	return	False
+
 expenses = pd.read_csv('expenses_cleaned.csv')
 out = []
 i = 0
@@ -31,7 +56,8 @@ for index, row in expenses.iterrows():
 			for lobbying_index, lobbying_row in lobbying_df.iterrows():
 				if lobbying_row['Employer'] == employer:
 					legislation_subject_matter = lobbying_row['Subject Matter of Proposed Legislation']
-					issue_bill_number = lobbying_row['Issue or bill number']
+					print(lobbying_row['Issue or bill number'])
+					issue_bill_number = clean_bills_list(lobbying_row['Issue or bill number'])
 					committee_lobbied = lobbying_row['Persons, Legislative committee or State Agency considering the matter']
 					break
 			# for item in page_soup:
@@ -45,7 +71,7 @@ for index, row in expenses.iterrows():
 	i += 1
 	if i % 500 == 0:
 		print(f'Scraped {i} lines.')
-	if i == 5000:
+	if i == 100:
 		print(f'Scraped {i * csvs} lines.  Printing to csv at {datetime.datetime.now()}')
 		pd.DataFrame(out).to_csv(f'scraped{csvs}.csv', index=True)
 		csvs += 1
