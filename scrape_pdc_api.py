5f8f79effc4853a1884bd27fb6e3a83e1ee2c920
Anthony Graffigna <graffigna.a@husky.neu.edu>
2024-02-23 14:00:51 -0500

    Add column names to df for records from db

--- scrape_pdc_api.py
+++ scrape_pdc_api.py
@@ -7,12 +7,38 @@ import psycopg2
 
 
 
+# Takes a raw scrape of a list of bills/issues as a string and returns a clean list of bills separated by a comma and a space
+def clean_bills_list(bills_raw):
+	print(bills_raw)
+	bills_upper = str(bills_raw).upper()
+	remove_hb_sb_spaces = bills_upper.replace("HB ", "HB").replace("SB ", "SB")
+	delimit_with_spaces = remove_hb_sb_spaces.replace(",", " ").replace("/", " ").replace(":", " ").replace(";", " ").replace("-", " ").replace(", ", " ").replace(")", "").replace("(", "")
+	bills_list = delimit_with_spaces.split(" ")
+	filter_nonnumbers = filter(filter_numbers, bills_list)
+	filter_nonhb_sb = filter(filter_hb_sb, filter_nonnumbers)
+	return list(filter_nonhb_sb)
+
+def filter_numbers(x):
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	for c in x:
+		if c in numbers:
+			return True
+	return False
+
+def filter_hb_sb(x):
+	if "HB" in x:
+		return True
+	elif "SB" in x:
+		return True
+	return	False
+
 # Takes an existing dataset and determines if the new row exists in it already
 def exists_in_dataset(new, existing):
 	return new['id'] in existing['id'].values
 
 # TODO: Add new item to dataset
 def add_to_dataset(new):
+	#print(f'ADDING {new} to dataset')
 	return 1
 
 # Unauthenticated client only works with public data sets. Note 'None'
@@ -38,14 +64,14 @@ connection = psycopg2.connect(database="pdc_data", user="postgres", password="pa
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT * from pdc limit 100;")
+cursor.execute("SELECT * from pdc ORDER BY receipt_date DESC limit 2000;")
+colnames = [desc[0] for desc in cursor.description]
 
 # Fetch all rows from database
 record = cursor.fetchall()
 
 record_df = pd.DataFrame.from_records(record)
-
-print("Data from Database:- ", record_df.head)
+record_df.columns = colnames
 
 for index, row in results_df.iterrows():
 	if exists_in_dataset(row, record_df):
